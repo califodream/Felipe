6694cac355cf304ac028438d7b0978aed4cdcc7f
huanzengwang <huanzengwang@linkcm.com>
2023-02-04 23:46:18 +0800

    chapter_5 Spring Data JPA

--- src/main/java/com/wong/Felipe/domain/ToDo.java
+++ src/main/java/com/wong/Felipe/domain/ToDo.java
@@ -2,17 +2,19 @@ package com.wong.Felipe.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+import org.hibernate.Hibernate;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.Objects;
 import java.util.UUID;
 
-@Data
-@Document
+@Entity
+@Getter
+@Setter
+@ToString
 public class ToDo {
     @NotNull
     @Id
@@ -21,21 +23,42 @@ public class ToDo {
     @NotBlank
     private String description;
     @JsonFormat(pattern = "yyyy:MM:dd HH:mm:ss", timezone = "GMT+8")
-    // @Column(updatable = false)
+    @Column(updatable = false)
     private LocalDateTime created;
     @JsonFormat(pattern = "yyyy:MM:dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime modified;
     private boolean completed;
 
     public ToDo() {
-        LocalDateTime localDateTime = LocalDateTime.now();
         this.id = UUID.randomUUID().toString();
-        this.created = localDateTime;
-        this.modified = localDateTime;
     }
 
     public ToDo(String description) {
         this();
         this.description = description;
+    }
+
+    @PrePersist
+    void OnCreate() {
+        this.setCreated(LocalDateTime.now());
+        this.setModified(LocalDateTime.now());
+    }
+
+    @PreUpdate
+    void OnUpdate() {
+        this.setModified(LocalDateTime.now());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        ToDo toDo = (ToDo) o;
+        return id != null && Objects.equals(id, toDo.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
     }
 }
