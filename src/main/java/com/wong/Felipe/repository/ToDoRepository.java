dea4bf797c9649a927a304b1c3da4760e1004684
huanzengwang <huanzengwang@linkcm.com>
2023-02-04 17:51:38 +0800

    chapter_6 Mono Example demo

--- src/main/java/com/wong/Felipe/repository/ToDoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wong.Felipe.repository;
-
-import com.wong.Felipe.domain.ToDo;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ToDoRepository extends CrudRepository<ToDo, String> {
-}
